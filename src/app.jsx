870f1173df15400307fc9fcfab3b3b17a90895fd
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-09 10:20:21 +0300

    Initial commit.

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './app.css';
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  return <div className="App">Hello App</div>;
 };
 
 export default App;
