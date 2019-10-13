6d02c2057f0e4c18cb57d62301be62558455d89b
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-13 13:57:31 +0300

    added AddLogModal and Preloader component

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/search-bar';
 import Logs from './components/logs/logs';
+import AddBtn from './components/layout/add-btn';
+import AddLogModal from './components/logs/add-log-modal';
 
 const App = () => {
   useEffect(() => {
@@ -13,6 +15,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
