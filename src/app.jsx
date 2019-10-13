989b825fc449067fb7b2b359960c5bfbc64a724a
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-13 12:59:24 +0300

    Added Materialize-CSS and Logs component

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import './app.css';
+import React, { useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/search-bar';
+import Logs from './components/logs/logs';
 
 const App = () => {
-  return <div className="App">Hello App</div>;
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
